3fd9826b889b86fde3ca6988f6a9dc47a36012bf
sasutona <jhonacesasutona718@gmail.com>
2026-01-19 19:32:38 +0800

    refactor: remove unused components and files

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,22 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import Navbar from "./Wrapper/Navbar"
-import Footer from "./Wrapper/Footer"
-import Home from "./pages/Home"
-import AboutUs from "./pages/AboutUs"
-import ContactUs from "./pages/ContactUs"
-import NotFound from "./pages/NotFound"
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Navbar />
         <div className="content">
           <Routes>
-
             <Route path="/" element={<Home />} />
 
-            <Route path="/About-Us" element={<AboutUs />} />
-
-            <Route path="/Contact-Us" element={<ContactUs />} />
-
             <Route path="*" element={<NotFound />} />
-
           </Routes>
         </div>
-        <Footer />
       </div>
     </Router>
-
-
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/AboutUs/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AboutUs = () => {
-  return (
-    <div>index</div>
-  )
-}
-
-export default AboutUs
